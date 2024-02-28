2f8a3e986e3cb446797991adba8d22ddc8a68c90
vurrau <podvishevskiy@gmail.com>
2024-02-28 21:19:54 +0200

    add Enum

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,15 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from src.db.base import Base
 
+from enum import Enum
+from sqlalchemy.dialects.postgresql import ENUM as PgEnum
+
+
+class UserRoleEnum(Enum):
+    USER = 'USER'
+    STAFF = 'STAFF'
+    ADMIN = 'ADMIN'
+
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -12,6 +21,8 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    role: Mapped[str] = mapped_column(
+        PgEnum(UserRoleEnum, name='user_role_enum', create_type=False), nullable=False, default=UserRoleEnum.USER)
 
 
 
