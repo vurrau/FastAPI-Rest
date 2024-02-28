d0145540dff4a6320cf105718b887336a8c91ad5
vurrau <podvishevskiy@gmail.com>
2024-02-28 21:10:27 +0200

    correct model

--- src/api/models.py
+++ src/api/models.py
@@ -1,29 +1,17 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP, func, JSON
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from src.db.base import Base
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    is_active = Column(Boolean, default=True, nullable=False)
-    registered_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
-    role_id = Column(Integer, ForeignKey('roles.id'))
-
-    role = relationship("Role", back_populates="users")
-
-
-class Role(Base):
-    __tablename__ = "roles"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-
-    users = relationship("User", back_populates="role")
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
 
 
