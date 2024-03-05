d7ed4ea15243f44a6660a2ac10a262a04c3d7fea
vurrau <podvishevskiy@gmail.com>
2024-03-05 20:55:33 +0200

    fin conftest, fix router/name register

--- src/config.py
+++ src/config.py
@@ -11,3 +11,9 @@ DB_PASS = os.environ.get("DB_PASS")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
 SECRET_MANAGER = os.environ.get("SECRET_MANAGER")
+
+DB_HOST_TEST = os.environ.get('DB_HOST_TEST')
+DB_USER_TEST = os.environ.get('DB_USER_TEST')
+DB_NAME_TEST = os.environ.get('DB_NAME_TEST')
+DB_PORT_TEST = os.environ.get('DB_PORT_TEST')
+DB_PASS_TEST = os.environ.get('DB_PASS_TEST')
--- src/main.py
+++ src/main.py
@@ -13,14 +13,14 @@ app = FastAPI()
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix="/auth",
     tags=["auth"],
 )
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth/jwt",
-    tags=["register"],
+    prefix="/auth",
+    tags=["auth"],
 )
 
 app.include_router(admin)
